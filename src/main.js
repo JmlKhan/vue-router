f0c7f229b8f7dbdcc8436a6822d14a377cc600bb
Jamal <jmlkhon17@gmail.com>
2021-11-11 10:47:40 +0500

    programmatic navigation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UserList from './components/users/UsersList';
 
 import App from './App.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UserList },
+  ],
+  linkActiveClass: 'active',
 });
 
 const app = createApp(App);
-
+4;
+app.use(router);
 app.mount('#app');
